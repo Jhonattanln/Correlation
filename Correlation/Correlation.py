9c57c362e36327c3b0d54f0daff87023cf49e690
Jhonattanln <63247251+Jhonattanln@users.noreply.github.com>
2020-08-25 14:38:37 -0300

    Update Correlation.py

--- Correlation/Correlation.py
+++ Correlation/Correlation.py
@@ -18,11 +18,31 @@ cumulative_returns = pd.DataFrame()
 for i in returns:
     cumulative_returns[i] = ((1+returns[i]).cumprod()-1)*100
 
-
+##Ploting data
 plt.style.use('ggplot')
 fig, ax = plt.subplots()
 ax.plot(cumulative_returns.index, cumulative_returns)
 ax.set_xlabel('Data')
 ax.set_ylabel('Retornos')
+plt.legend(['CPLE6', 'SAPR11', 'KLBN11', 'RAIL3', 'POSI3', 'BTTL3', 'PRBR11'])
+plt.show()
+
+##Ploting correlation
+correl = returns.corr()
+sns.heatmap(correl, annot=True, cbar=False, cmap='RdBu_r')
+plt.yticks(rotation=0)
+plt.xticks(rotation=90)
+plt.show()
+
+#Rolling correlation
+roll_corrl = pd.DataFrame()
+for i in returns:
+    roll_corrl[i] = returns[i].rolling(60).corr(returns['PRBR11'])
+
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+ax.plot(roll_corrl.index, roll_corrl)
+ax.set_xlabel('Data')
+ax.set_ylabel('Correlação')
 plt.legend(['CPLE6', 'SAPR11', 'KLBN11', 'RAIL3', 'POSI3', 'BTTL3', 'PRBR11'])
 plt.show()
